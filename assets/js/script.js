0660af03be35f737cb1429bc1cdeac86b6019342
lean532 <leandrojcnunez@gmail.com>
2023-11-29 20:00:27 -0300

    Api y estilos Lean

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,13 @@
-//DETECTAR LA PAGINA ACTIVA (hay efecto en css)
-
-// Conseguir la URL actual
-var url = window.location.href;
-
-// Conseguir todos los elementos <a> de la barra de navegación
-var links = document.querySelectorAll(".nav-menu a");
-
-// Recorro cada elemento
-for (var i = 0; i < links.length; i++) {
-  // Si la URL del enlace coincide con la URL actual
+let url = window.location.href;
+let links = document.querySelectorAll(".nav-menu a");
+for (let i = 0; i < links.length; i++) {
   if (links[i].href === url) {
-    // Agrega la clase 'active' al elemento li padre del enlace
     links[i].parentNode.classList.add("active");
-
-    // Agrega un listener al elemento para prevenir que se haga click
     links[i].addEventListener("click", function (event) {
       event.preventDefault();
     });
   }
 }
-
-//APARECER/DESAPARECER EL MENU HAMBURGUESA (nav-menu y nav-social)
 const navMenu = document.querySelector("#navMenu");
 const navSocial = document.querySelector("#navSocial");
 const abrir = document.querySelector("#abrir");
@@ -36,20 +23,18 @@ cerrar.addEventListener("click", () => {
 });
 
 const section = document.querySelector(".bar-start");
-
-button.addEventListener("click", () => {
-  section.classList.toggle("active");
-});
-
-//VALIDACIONES DEL FORMULARIO
+let button = document.querySelector("button");
+if (button) {
+  button.addEventListener("click", () => {
+    section.classList.toggle("active");
+  });
+}
 function validarFormulario() {
-  var nombre = document.getElementById("nombre").value;
-  var apellido = document.getElementById("apellido").value;
-  var dni = document.getElementById("dni").value;
-  var correo = document.getElementById("correo").value;
-  var password = document.getElementById("password").value;
-
-  // Validar que los campos no estén vacíos
+  let nombre = document.getElementById("nombre").value;
+  let apellido = document.getElementById("apellido").value;
+  let dni = document.getElementById("dni").value;
+  let correo = document.getElementById("correo").value;
+  let password = document.getElementById("password").value;
   if (
     nombre.trim() == "" ||
     apellido.trim() == "" ||
@@ -60,22 +45,17 @@ function validarFormulario() {
     alert("Todos los campos son obligatorios.");
     return false;
   }
-
-  // Validar el DNI
   if (isNaN(dni) || dni < 0) {
     alert("Por favor, ingrese un DNI válido.");
     return false;
   }
 
-  // Validar el correo electrónico
-  var regexCorreo = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+  let regexCorreo = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
   if (!regexCorreo.test(correo)) {
     alert("Por favor, ingrese un correo electrónico válido.");
     return false;
   }
-
-  // Mostrar un cartel de confirmación con los datos ingresados
-  var confirmacion = confirm(
+  let confirmacion = confirm(
     "Por favor, revise los datos ingresados:\n\n" +
       "Nombre: " +
       nombre +
@@ -91,15 +71,11 @@ function validarFormulario() {
       "\n\n" +
       "¿Son correctos estos datos?"
   );
-
-  // Si el usuario acepta, se envía el formulario
   return confirmacion;
 }
-
-// Función para enviar el correo electrónico
 function enviarCorreo() {
   console.log("Entra a enviar correo");
-  var correo = document.getElementById("correo").value;
+  let correo = document.getElementById("correo").value;
 
   Email.send({
     Host: "smtp.gmail.com",
@@ -111,14 +87,84 @@ function enviarCorreo() {
     Body: "Gracias por tu compra. Te esperamos en el concierto.",
   }).then((message) => alert("Correo enviado exitosamente"));
 }
+let form = document.querySelector("form");
+if (form) {
+  form.addEventListener("submit", function (event) {
+    console.log("Entra a validar formulario");
+    event.preventDefault();
 
-// Asignar la función de validación al evento de envío del formulario
-document.querySelector("form").addEventListener("submit", function (event) {
-  console.log("Entra a validar formulario");
-  event.preventDefault();
+    if (validarFormulario()) {
+      enviarCorreo();
+      this.submit();
+    }
+  });
+}
+let container = document.getElementById("container_reviews");
+fetch("https://rickandmortyapi.com/api/character")
+  .then((response) => response.json())
+  .then((data) => {
+    let fragment = document.createDocumentFragment();
 
-  if (validarFormulario()) {
-    enviarCorreo();
-    this.submit();
-  }
-});
+    data.results.forEach((character) => {
+      let characterContainer = document.createElement("div");
+      characterContainer.className = "character-container";
+
+      let img = document.createElement("img");
+      img.src = character.image;
+      img.alt = character.name;
+      img.className = "img-promo";
+
+      let pName = document.createElement("p");
+      pName.textContent = character.name;
+
+      let review = Math.floor(Math.random() * 5) + 1;
+      let reviewText = "";
+      switch (review) {
+        case 1:
+          reviewText = "Muy malo 👎🏻";
+          break;
+        case 2:
+          reviewText = "Malo";
+          break;
+        case 3:
+          reviewText = "Regular";
+          break;
+        case 4:
+          reviewText = "Muy bueno";
+          break;
+        case 5:
+          reviewText = "¡Excelente! 🎉";
+          break;
+      }
+
+      let stars = "";
+      for (let i = 0; i < review; i++) {
+        stars += "⭐";
+      }
+
+      let pStars = document.createElement("p");
+      pStars.textContent = stars;
+
+      let pReview = document.createElement("p");
+      pReview.textContent = reviewText;
+
+      characterContainer.appendChild(img);
+      characterContainer.appendChild(pName);
+      characterContainer.appendChild(pStars);
+      characterContainer.appendChild(pReview);
+
+      fragment.appendChild(characterContainer);
+    });
+
+    container.appendChild(fragment);
+  })
+  .catch((error) => console.error("Error:", error));
+
+let h2 = document.querySelector(".photos h2");
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+let i = 0;
+
+setInterval(function () {
+  h2.style.color = colors[i];
+  i = (i + 1) % colors.length;
+}, 500);
