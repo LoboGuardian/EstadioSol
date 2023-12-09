4fc36299fcb2314d41c95259a4359157695c5321
NahiDenice <nahiara.alegre@hotmail.com>
2023-12-09 17:48:34 -0300

    Agergados los comments de nuevo en el JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
+//DETECTAR LA PAGINA ACTIVA (hay efecto en css)
+
+// Conseguir la URL actual
 let url = window.location.href;
+
+// Conseguir todos los elementos <a> de la barra de navegación
 let links = document.querySelectorAll(".nav-menu a");
+
+//Recorro cada elemento
 for (let i = 0; i < links.length; i++) {
+  //Si la URL del enlace coincide con la URL actual
   if (links[i].href === url) {
+    //Agrega la clase 'active' al elemento li padre del enlace
     links[i].parentNode.classList.add("active");
+
+    //Agrega un listener al elemento para prevenir que se haga click
     links[i].addEventListener("click", function (event) {
       event.preventDefault();
     });
   }
 }
+
+//APARECER/DESAPARECER EL MENU HAMBURGUESA (nav-menu y nav-social)
 const navMenu = document.querySelector("#navMenu");
 const navSocial = document.querySelector("#navSocial");
 const abrir = document.querySelector("#abrir");
@@ -23,18 +36,25 @@ cerrar.addEventListener("click", () => {
 });
 
 const section = document.querySelector(".bar-start");
+
 let button = document.querySelector("button");
 if (button) {
   button.addEventListener("click", () => {
     section.classList.toggle("active");
   });
 }
+
+
+
+//VALIDACIONES DEL FORMULARIO
 function validarFormulario() {
   let nombre = document.getElementById("nombre").value;
   let apellido = document.getElementById("apellido").value;
   let dni = document.getElementById("dni").value;
   let correo = document.getElementById("correo").value;
   let password = document.getElementById("password").value;
+  
+  //Validar que los campos no estén vacíos
   if (
     nombre.trim() == "" ||
     apellido.trim() == "" ||
@@ -45,16 +65,21 @@ function validarFormulario() {
     alert("Todos los campos son obligatorios.");
     return false;
   }
+
+  //Validar el DNI
   if (isNaN(dni) || dni < 0) {
     alert("Por favor, ingrese un DNI válido.");
     return false;
   }
 
+  //Validar el correo electrónico
   let regexCorreo = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
   if (!regexCorreo.test(correo)) {
     alert("Por favor, ingrese un correo electrónico válido.");
     return false;
   }
+
+  // Mostrar un cartel de confirmación con los datos ingresados
   let confirmacion = confirm(
     "Por favor, revise los datos ingresados:\n\n" +
       "Nombre: " +
@@ -71,8 +96,20 @@ function validarFormulario() {
       "\n\n" +
       "¿Son correctos estos datos?"
   );
-  return confirmacion;
+  return confirmacion;   // Si el usuario acepta, se envía el formulario
 }
+
+/* Asignar la función de validación al evento de envío del formulario
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (validarFormulario()) {
+    this.submit();
+  }
+});
+*/
+
+
 function enviarCorreo() {
   console.log("Entra a enviar correo");
   let correo = document.getElementById("correo").value;
